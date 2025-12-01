cdcc7fd5d04a702acc360984b775d0896c37699a
shalvi2133-create <shalvi2133@gmail.com>
2025-12-02 01:28:50 +0530

    Add files via upload

---
+++ q43.c
@@ -0,0 +1,33 @@
+// Write a program to check if a number is a strong number.
+#include <stdio.h>
+
+// Function to calculate factorial
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+        fact *= i;
+    return fact;
+}
+
+int main() {
+    int num, temp, sum = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while (temp != 0) {
+        digit = temp % 10;
+        sum += factorial(digit);
+        temp /= 10;
+    }
+
+    if (sum == num) {
+        printf("Strong number");
+    } else {
+        printf("Not strong number");
+    }
+
+    return 0;
+}
