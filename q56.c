7e909950327f2ce7e9e6940172a9e0881d7b62e8
shalvi2133-create <shalvi2133@gmail.com>
2025-12-02 01:43:32 +0530

    Add files via upload

---
+++ q56.c
@@ -0,0 +1,26 @@
+// Read and print elements of a one-dimensional array.
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];  
+
+
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    printf("Array elements: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
