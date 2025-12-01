2ba0e4a82e10bbb8d212aa831665bcb5ef94e08d
shalvi2133-create <shalvi2133@gmail.com>
2025-12-02 01:40:03 +0530

    Add files via upload

---
+++ q51.c
@@ -0,0 +1,26 @@
+// Write a program to print the following pattern:
+    /*5
+   45
+  345
+ 2345
+12345*/
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {              // loop for rows
+        
+        for (int s = 1; s <= rows - i; s++) {
+            printf(" ");
+        }
+        
+        for (int j = rows - i + 1; j <= rows; j++) {
+            printf("%d", j);
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
+
