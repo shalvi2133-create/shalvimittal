bba56712d63382b123ee591783b32486af7f30d5
shalvi2133-create <shalvi2133@gmail.com>
2025-12-03 11:20:57 +0530

    Add files via upload

---
+++ q82.c
@@ -0,0 +1,16 @@
+// Print each character of a string on a new line.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    
+    scanf("%s", str); // read string without spaces
+
+    int i = 0;
+    while (str[i] != '\0') {
+        printf("%c\n", str[i]);
+        i++;
+    }
+
+    return 0;
+}
